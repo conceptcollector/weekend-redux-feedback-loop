c4cc95b32b9d685f63500ea52eaab47ec9044caf
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-05-07 16:03:43 -0500

    Running into the same problem from class with push being undefined, moving onto something else...

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,24 @@
-import { HashRouter as Router, Link } from 'react-router-dom';
+import {HashRouter as Router, Route, Link, useHistory} from 'react-router-dom';
 import {useState} from 'react';
 import './App.css';
 import {useDispatch} from 'react-redux';
+import UnderstandingForm from '../UnderstandingForm/UnderstandingForm';
 
 function App() {
 
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const [feeling, setFeeling] = useState('');
 
   const handleSubmission = (e) => {
+    console.log('In handleSubmission', feeling);
     e.preventDefault();
     dispatch({
       type: 'ADD_TO_SUMMARY',
       payload: feeling
     })
+    history.push('/UnderstandingForm');
   }
 
   return (
@@ -25,17 +29,19 @@ function App() {
           <h4>Don't forget it!</h4>
         </header>
         <h1>How are you feeling today?</h1>
-        <form onSubmit={handleSubmission} className="">
+        <form>
           <input
             required
             placeholder="0-6"
             value={feeling}
             onChange={(event) => setFeeling(event.target.value)}
           />
+          <Link to="/UnderstandingForm">   
+            <button onClick={handleSubmission}>Next</button>
+          </Link>
+          <Route exact path="/UnderstandingForm">
+          </Route>
         </form>
-        <Link to="/UnderstandingForm">   
-          <button>Next</button>
-        </Link>
       </div>
     </Router>
   );
---
+++ src/components/UnderstandingForm/UnderstandingForm.jsx
@@ -0,0 +1,14 @@
+import '../App/App.css';
+
+function UnderstandingForm() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+            <h1 className='App-title'>Feedback!</h1>
+            <h4>Don't forget it!</h4>
+            </header>
+        </div>
+    )
+}
+
+export default UnderstandingForm;
